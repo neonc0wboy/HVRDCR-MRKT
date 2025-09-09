ed2f0c8e3443039379c45d43072c8e35e5b066e2
c0wb0y <nikita.mailml@gmail.com>
2025-09-09 03:53:00 +0300

    upd 3

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,9 +31,7 @@ const Header: React.FC = () => {
                     <div className="hidden md:flex items-center gap-4">
                         <NavLink to="/" className={navLinkClasses}>CPU</NavLink>
                     </div>
-
                 </div>
-                    <a> <img alt="Logo" src="/favicons/logo.png" style="max-width: auto; height: auto;"></img></a>
 
                 <div className="flex items-center gap-4">
                     <NavLink to="/cart" className="relative text-gray-300 hover:text-white transition-colors">
